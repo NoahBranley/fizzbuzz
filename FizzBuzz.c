a8022d6dd3baea81aaed863189a1460e86b8ed94
poopoo <112705279+NoahBranley@users.noreply.github.com>
2023-11-02 18:27:24 +0800

    Add files via upload

---
+++ FizzBuzz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    for(int i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0)
+        printf("fizz");
+
+            if (i % 5 == 0)
+            printf("buzz");
+
+                if ((i % 3 != 0) && (i % 5 != 0))
+                printf("%d", i);
+                printf("\n");
+    }
+return 0;
+}
